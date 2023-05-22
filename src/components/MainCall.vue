58150373632ef2db629b705d4e1ce3434eeaaa94
Tatiana Borda <tatiana0borda@gmail.com>
2023-05-22 12:23:38 +0200

    Some configs for windows | Tring axios for getting data

---
+++ src/components/MainCall.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  mounted(){
+      axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY')
+        .then(response => console.log(response))
+    }
+}
+</script>
